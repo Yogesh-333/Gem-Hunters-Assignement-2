5bb9bf5aaadeeea29b4a43c061feaba0d6e8a289
Yogesh Kumar <yogeshgopal98@gmail.com>
2024-06-10 21:46:25 -0400

    Initial setups and player file

---
+++ Gem Hunters/Gem Hunters/Player.cs
@@ -0,0 +1,15 @@
+﻿public class Player
+{
+    public string Name { get; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+    }
+
+   
+}
---
+++ Gem Hunters/Gem Hunters/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Game gemHunters = new Game();
+        gemHunters.Start();
+    }
+}
